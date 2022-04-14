0d60726e47d665ebe08df8a721d97bd9fd963b54
WangZf <wangzf@wangzifengdeMacBook-Pro.local>
2022-04-14 10:20:54 +0800

    保存标签页组

--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,13 +1,29 @@
 <template>
-  <el-container style="width:600px; height: 400px; border-radius: 8px; background:#9AC8E280;">
-    <el-header style="text-align: right; font-size: 12px">
-      <el-row>
-        <el-button-group>
-          <el-button type="primary" icon="el-icon-edit"></el-button>
-          <el-button type="primary" icon="el-icon-share"></el-button>
-          <el-button type="primary" icon="el-icon-delete"></el-button>
-        </el-button-group>
-        <el-input v-model="search_input" placeholder="搜索"></el-input>
+  <el-container style="width:400px; height: 500px; border-radius: 8px; background:#9AC8E280;">
+    <el-header style="text-align: right; font-size: 12px; height: 100px;" >
+      <el-row :gutter="20">
+        <el-col :span="8">
+          <el-image v-if = "!isGif"
+          @mouseenter="isGif = true"
+          @mouseleave="isGif = false" 
+          style="width: 100px; height: 100px"
+          :src="pngurl"
+          fit="fill"></el-image>
+          <el-image v-else
+          @mouseenter="isGif = true"
+          @mouseleave="isGif = false" 
+          style="width: 100px; height: 100px"
+          :src="gifurl"
+          fit="fill"></el-image>
+        </el-col>
+        <el-col :span="16">
+          <el-button-group>
+            <el-button type="primary" icon="el-icon-edit"></el-button>
+            <el-button type="primary" icon="el-icon-share"></el-button>
+            <el-button type="primary" icon="el-icon-delete"></el-button>
+          </el-button-group>
+          <el-input v-model="search_input" placeholder="搜索"></el-input>
+        </el-col>
       </el-row>
     </el-header>
     <el-main>
@@ -20,13 +36,13 @@
           </el-carousel>
         </el-row>
       </el-card>
-      <el-card shadow="hover" style="border-radius: 8px;0">
+      <el-card shadow="hover" style="border-radius: 8px;">
         <el-row>
           <el-button icon="el-icon-close-notification" circle></el-button>
-          <el-button icon="el-icon-picture" circle @click="screenshot"></el-button>
-          <el-button icon="el-icon-document-copy" circle></el-button>
+          <el-button icon="el-icon-picture" circle></el-button>
+          <el-button icon="el-icon-document-copy" circle @click="drawer = true"></el-button>
           <el-button icon="el-icon-eleme" circle></el-button>
-          <el-button icon="el-icon-download" circle></el-button>
+          <el-button icon="el-icon-download" circle @click="getTabs"></el-button>
           <!-- <el-button icon="el-icon-circle-close" circle></el-button> -->
         </el-row>
         <el-row>
@@ -34,33 +50,102 @@
           <el-button icon="el-icon-date" circle></el-button>
           <el-button icon="el-icon-finished" circle></el-button>
           <el-button icon="el-icon-notebook-2" circle></el-button>
-          <el-button icon="el-icon-s-data" circle></el-button>
+          <el-button icon="el-icon-s-data" circle @click="reset"></el-button>
         </el-row>
       </el-card>
+
+      <el-drawer
+        title="tabs reload"
+        size="300px"
+        :with-header=false
+        :visible.sync="drawer"
+        direction="ttb"
+        :destroy-on-close=true
+        @open="getSavedTabsGroupList">
+        <el-divider><i class="el-icon-folder-add"></i>  存储</el-divider>
+        <el-form :inline="true" :model="saveTabsGroupForm" size="medium" label-width="120px">
+          <el-form-item label="标签组名">
+            <el-input v-model="saveTabsGroupForm.groupName" placeholder="需存储的标签组名"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="saveTabsGroup">保存</el-button>
+          </el-form-item>
+        </el-form>
+        <el-divider><i class="el-icon-setting"></i>  加载或删除</el-divider>
+        <el-form :model="reloadTabsGroupForm" size="medium" label-width="120px">
+          <el-form-item label="标签组名">
+            <el-select v-model="reloadTabsGroupForm.groupName" placeholder="需要加载的标签组">
+              <el-option v-for="(vakue, key, index) in savedTabsGroupList" :key="index" :value="key"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary"  @click="reloadTabsGroup">加载</el-button>
+            <el-button @click="deleteTabsGroup">删除</el-button>
+          </el-form-item>
+        </el-form>
+      </el-drawer>
+
     </el-main>
   </el-container>
 </template>
 
 <script>
-// import chrome from "vue-cli-plugin-chrome-ext"
-
   export default {
     name: 'app',
     data() {
       return {
-        search_input:"",
+        isGif: false,
+        pngurl: 'assets/start.png',
+        gifurl: 'assets/start.gif',
+        search_input: '',
+        savedTabsGroupList: {},
+        saveTabsGroupForm: {groupName: ''},
+        reloadTabsGroupForm: {groupName: ''},
+        drawer: false,
       };
     },
     methods: {
       screenshot () {
-        // chrome.tabs.captureVisibleTab(null,{format:"png",quality:100},function (screenshot){
-        //   chrome.downloads.download({url:screenshot,saveAs:true},(res)=>{console.log(res)})
-        //   console.log(screenshot.data.toDataURL())
-        // });
         chrome.tabs.captureVisibleTab(null, {format: "png", quality: 100}, (img_url) => {
-          chrome.downloads.download({url: img_url, saveAs: true}, (res) => {console.log(res)})
+          chrome.downloads.download({url: img_url, saveAs: true}, (res) => {console.log(res)});
         });
       },
+      getTabs () {
+        chrome.tabs.query({}, (tabs) => {
+          var urls = tabs.map((tab) => {return tab.url});
+          chrome.storage.local.set({"test":urls});
+        });
+      },
+      getSavedTabsGroupList () {
+        chrome.storage.local.get("savedTabsGroupList", (entry) => {
+          this.savedTabsGroupList = (typeof(entry.savedTabsGroupList) == 'undefined') ? {} : entry.savedTabsGroupList;
+          });
+      },
+      saveTabsGroup () {
+        chrome.tabs.query({}, (tabs) => {
+          var urls = tabs.map((tab) => {return tab.url});
+          this.savedTabsGroupList[this.saveTabsGroupForm.groupName] = urls;
+          chrome.storage.local.set({"savedTabsGroupList":this.savedTabsGroupList});
+        });
+      },
+      reloadTabsGroup () {
+        chrome.storage.local.get("savedTabsGroupList", (entry) => {
+          entry.savedTabsGroupList[this.reloadTabsGroupForm.groupName].forEach(URL => {chrome.tabs.create({url:URL})});
+        });
+      },
+      reset () {
+        chrome.storage.local.clear(() => {console.log("clear")})
+      },
+      deleteTabsGroup () {
+        console.log(this.savedTabsGroupList);
+        chrome.storage.local.get("savedTabsGroupList", (entry) => {
+          this.savedTabsGroupList = (typeof(entry.savedTabsGroupList) == 'undefined') ? {} : entry.savedTabsGroupList;
+          delete entry.savedTabsGroupList[this.reloadTabsGroupForm.groupName];
+          console.log(this.savedTabsGroupList)
+          chrome.storage.local.set({"savedTabsGroupList":this.savedTabsGroupList});
+        });
+      }
+
     }
   };
 </script>
